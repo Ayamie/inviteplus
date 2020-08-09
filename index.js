1c91a3b4467251cd5c435f536d543278b318acff
jordanoffshl <69113217+jordanoffshl@users.noreply.github.com>
2020-08-08 22:09:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ client.on("message", message => {
     if(message.content === `${prefix}ping`) {
         message.channel.send(`🏓 pong - ${client.ws.ping}`)
     }
+    
+    if(message.content === `salut`)
+        message.channel.send('salut');
 })
 
 client.login(process.env.TOKEN);
