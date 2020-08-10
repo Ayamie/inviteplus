c690540467d987832a975a1bb8080a5f4e474560
jordanoffshl <69113217+jordanoffshl@users.noreply.github.com>
2020-08-10 09:31:46 -0400

    Update invitePlus.js

--- invitePlus.js
+++ invitePlus.js
@@ -1,23 +1,94 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require('./config.json');
+const prefix = "!";
 client.on("ready", message => {
-    console.log('salut je suis prêt');
-
-});
-
-client.on("message", message => {
-    if(message.content === `${prefix}ping`) {
-        message.channel.send(`🏓 pong - ${client.ws.ping}`)
-    }
+    console.log('bot => ok');
+          
 })
 
-const { prefix, token } = require('./config.json'); 
-const client = new Discord.Client(); 
-client.commands = new Discord.Collection(); 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js')); 
-for (const file of commandFiles) { 	
-const command = require(`./commands/${file}`); 
-client.commands.set(command.name, command); }
+client.on("message", message => {
+    if(message.content === `${prefix}ping`) {
+        message.channel.send(`🏓 pong - ${client.ws.ping}`)
+    }
+        
+    if(message.content === `hello`){
+        message.channel.send('bonjour<a:valide:731232586411540552>');
+    }
+        
+    if(message.content === `!help`){
+         var help = '!ping => pong !'
+                     '!hello => bonjour!'
+                      'studio => la music'
+                     
+         message.channel.send(help)  
+   } 
 
-client.login('');
+   if(message.content === `tg`){
+        message.channel.send('merci de ne pas insulter ');
+    }
+
+   if(message.content === `Fdp`){
+        message.channel.send('ta mère la pute tu arrête <a:ban:736946398490001408>');
+    }
+
+   if(message.content === `giveaway nitro`){
+        message.channel.send('1 invite = nitro classic  <a:emoji_55:739952349937664131>!');
+                             
+    }
+    
+if (message.content === "!embed"){
+let embed = new Discord.MessageEmbed()
+.setTitle('Hello! :)')
+.setDescription("**moderations**")
+.addField( 'ban,kick,bantemporaire')       
+.setThumbnail('https://i.imgur.com/wSTFkRM.png')
+.setImage('https://i.imgur.com/wSTFkRM.png')
+.setColor('#020202')
+.setFooter('jordan_offshl', 'https://cdn.discordapp.com/attachments/737388526848442459/742075728358539355/4713_ubot-1.png');
+
+message.channel.send(embed);
+ 
+    }
+if(message.content === `!wl`){
+    message.reply("vient d'avoir accès à inviteManager");
+      
+   }
+      
+if (message.content === `!ticket`){
+let embed = new Discord.MessageEmbed()
+.setTitle('') 
+.setDescription('username hello welcome in the team!')
+.setImage('https://cdn.discordapp.com/attachments/714924113713561660/717739024478896198/8881444f23794650da1fe33c1bed39e0.gif');
+message.channel.send(embed);
+
+   } 
+if (message.content === `!mod`){
+let embed = new Discord.MessageEmbed()
+.setTitle('voici mes commands !help') 
+.setDescription('<===Invitation==>')
+.addField('!help')
+.addField('**<===moderation==>**');
+.setImage('https://cdn.discordapp.com/attachments/719369502105665599/742136383329665044/c16009efd53f10bba79f19c683d53c3e.gif');
+.setTimestamp()
+.setFooter('create by jordan');
+
+message.channel.send(embed);
+
+   }
+   
+ if(message.content === `.info`){
+ if(message.content === `.info`)
+let embed = new Discord.MessageEmbed()
+.setTitle('🔧inviteMaster info🔧')
+.addField(
+	{ name: 'ANTI BOT', value: '`on`', inline: true },
+  { name: '\u200B', value: '\u200B' },
+	{ name: 'ANTI RÔLE', value: '`on`', inline: true },
+	{ name: 'ANTI MESSAGE BAN', value: '`on`', inline: true },
+	{ name: 'ANTI MESSAGE KICK', value: '`on`', inline: true },
+)
+.setFooter('inviteMaster', 'https://cdn.discordapp.com/attachments/737388526848442459/742203444919533668/images_14.png');  
+    }
+
+})
+client.login(process.env.TOKEN);
